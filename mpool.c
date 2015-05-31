c6ba5875c241c75452098a6bf686f01eb6c1f5e5
klopp <klopp@yandex.ru>
2015-05-31 08:53:29 +0300

    cosmetic

--- mpool.c
+++ mpool.c
@@ -8,6 +8,21 @@
 #include "mpool.h"
 #include <string.h>
 
+/*
+     mpool.pool structure:
+
+     +---- struct _mblk ---+  allocated +---- struct _mblk ---+  allocated +---
+     |                     |   memory   |                     |    memory  |
+     +--------+-----+------+------------+--------+-----+------+------------+---
+     | 0x1515 | 0/1 | size | size_bytes | 0x1515 | 0/1 | size | size_bytes |
+     +--------+-----+------+------------+--------+-----+------+------------+---
+         |       |      |        |
+         |       |      |        +---- memory returned by mp_alloc()
+         |       |      +---- block size
+         |       +---- free/busy
+         +---- block signature
+*/
+
 #define MP_VALID( ptr, mp ) \
     ((char *)(ptr) >= (mp)->min && \
     (char *)(ptr) <= (mp)->max && \
