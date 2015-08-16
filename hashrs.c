31b7a18d5003e2b06efd50a1613ef30c6e78d356
klopp <klopp@yandex.ru>
2015-08-16 06:08:06 +0300

    ((unsigned char *)buf)++

--- hashrs.c
+++ hashrs.c
@@ -16,7 +16,7 @@ static unsigned hash = 0;
 unsigned hash_rs( unsigned startval, const void * buf, size_t size )
 {
     if( !startval ) a = A;
-    for( hash = startval; size; size--, (unsigned char *)buf++ )
+    for( hash = startval; size; size--, ((unsigned char *)buf)++ )
     {
         hash = hash * a + *((unsigned char *)buf);
         a *= B;
