afff97d73be3051f586c7c96e493687400bf93f6
klopp <klopp@yandex.ru>
2015-08-17 21:09:19 +0300

    buf = (unsigned char  *)buf + 1;

--- hashrot13.c
+++ hashrot13.c
@@ -11,10 +11,11 @@ static unsigned hash = 0;
 
 unsigned hash_rot13( unsigned startval, const void * buf, size_t size )
 {
-    for( hash = startval; size; size--, (unsigned char  *)buf++ )
+    for( hash = startval; size; size-- )
     {
         hash += *((unsigned char *)buf);
         hash -= (hash << 13) | (hash >> 19);
+        buf = (unsigned char  *)buf + 1;
     }
     return hash;
 }
