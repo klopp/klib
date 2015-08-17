5efda96539131e6610112d58d5076863084b011e
klopp <klopp@yandex.ru>
2015-08-17 21:09:44 +0300

    buf = (unsigned char  *)buf + 1;

--- hashly.c
+++ hashly.c
@@ -14,8 +14,11 @@ static unsigned hash = 0;
 
 unsigned hash_ly( unsigned startval, const void * buf, size_t size )
 {
-    for( hash = startval; size; size--, (unsigned char *)buf++ )
+    for( hash = startval; size; size-- )
+    {
         hash = (hash * A) + *((unsigned char *)buf) + B;
+        buf = (unsigned char  *)buf + 1;
+    }
     return hash;
 }
 
