d0bfc84e9f262d2a7b60ee5b21049654be98fec1
klopp <klopp@yandex.ru>
2015-09-05 01:20:29 +0300

    added

---
+++ uniqid.c
@@ -0,0 +1,27 @@
+/*
+ * uniqid.c, part of "klib" project.
+ *
+ *  Created on: 05.09.2015, 01:08
+ *      Author: Vsevolod Lutovinov <klopp@yandex.ru>
+ */
+
+#include "uniqid.h"
+#include <sys/time.h>
+#include <stdio.h>
+
+char * uniqid( char * storage, size_t storage_len )
+{
+    struct timeval tv;
+    unsigned int sec, usec;
+    if( !storage )
+    {
+        storage_len = 8 + 5;
+        storage = Malloc( storage_len +1 );
+        if( !storage ) return NULL;
+    }
+    gettimeofday( &tv, NULL );
+    sec = (unsigned int)tv.tv_sec;
+    usec = ((unsigned int)tv.tv_usec % 0x100000);
+    snprintf( storage, storage_len, "%08x%05x", sec, usec );
+    return storage;
+}
---
+++ uniqid.h
@@ -0,0 +1,15 @@
+/*
+ * uniqid.h, part of "klib" project.
+ *
+ *  Created on: 05.09.2015, 01:07
+ *      Author: Vsevolod Lutovinov <klopp@yandex.ru>
+ */
+
+#ifndef UNIQID_H_
+#define UNIQID_H_
+
+#include "config.h"
+
+char * uniqid( char * storage, size_t storage_len );
+
+#endif /* UNIQID_H_ */
