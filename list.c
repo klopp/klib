69711cc741b384e342179c2c75dfddae85ebbe42
klopp <klopp@yandex.ru>
2016-06-07 18:26:22 +0300

    lock added

--- list.c
+++ list.c
@@ -7,160 +7,164 @@
 
 #include "list.h"
 
-List lcreate( L_destructor destructor )
-{
-    List list = Calloc( sizeof(struct _List), 1 );
-    if( !list ) return NULL;
+#include "_lock.h"
+
+List lcreate(L_destructor destructor) {
+    List list = Calloc(sizeof(struct _List), 1);
+    if (!list)
+        return NULL;
     list->destructor = destructor;
     return list;
 }
-void lclear( List list )
-{
-    if( list )
-    {
+
+void lclear(List list) {
+    if (list) {
+        _lock(&list->lock);
         LNode node = list->head;
-        while( node )
-        {
+        while (node) {
             LNode current = node;
             node = node->next;
-            if( list->destructor && current->data ) list->destructor(
-                    current->data );
-            Free( current );
+            if (list->destructor && current->data)
+                list->destructor(current->data);
+            Free(current);
         }
         list->head = list->tail = list->cursor = NULL;
         list->size = 0;
+        _unlock(list->lock);
     }
 }
 
-void ldestroy( List list )
-{
-    lclear( list );
-    Free( list );
+void ldestroy(List list) {
+    lclear(list);
+    Free(list);
 }
 
-void * ladd( List list, void * data )
-{
-    if( list && data )
-    {
-        LNode node = Calloc( sizeof(struct _LNode), 1 );
-        if( !node ) return NULL;
+void * ladd(List list, void * data) {
+    if (list && data) {
+        _lock(&list->lock);
+        LNode node = Calloc(sizeof(struct _LNode), 1);
+        if (!node) {
+            _unlock(list->lock);
+            return NULL;
+        }
         node->data = data;
-        if( !list->head )
-        {
+        if (!list->head) {
             list->head = list->tail = node;
         }
-        else
-        {
+        else {
             node->prev = list->tail;
             list->tail->next = node;
             list->tail = list->tail->next;
         }
         list->size++;
+        _unlock(list->lock);
         return node->data;
     }
     return NULL;
 }
 
-void * lpoke( List list, void * data )
-{
-    if( list && data )
-    {
-        LNode node = Calloc( sizeof(struct _LNode), 1 );
-        if( !node ) return NULL;
+void * lpoke(List list, void * data) {
+    if (list && data) {
+        _lock(&list->lock);
+        LNode node = Calloc(sizeof(struct _LNode), 1);
+        if (!node) {
+            _unlock(list->lock);
+            return NULL;
+        }
         node->data = data;
-        if( !list->head )
-        {
+        if (!list->head) {
             list->head = list->tail = node;
         }
-        else
-        {
+        else {
             node->next = list->head;
             list->head->prev = node;
             list->head = node;
         }
         list->size++;
+        _unlock(list->lock);
         return node->data;
     }
     return NULL;
 }
 
-void * lfirst( List list )
-{
-    if( !list ) return NULL;
+void * lfirst(List list) {
+    if (!list)
+        return NULL;
     list->cursor = list->head;
     return list->cursor ? list->cursor->data : NULL;
 }
 
-void * lnext( List list )
-{
-    if( !list || !list->cursor ) return NULL;
+void * lnext(List list) {
+    if (!list || !list->cursor)
+        return NULL;
     list->cursor = list->cursor->next;
     return list->cursor ? list->cursor->data : NULL;
 }
 
-void lwalk( List list, L_walk walker )
-{
-    if( list && walker )
-    {
+void lwalk(List list, L_walk walker) {
+    if (list && walker) {
+        _lock(&list->lock);
         LNode node = list->head;
-        while( node )
-        {
-            walker( node->data );
+        while (node) {
+            walker(node->data);
             node = node->next;
         }
+        _unlock(list->lock);
     }
 }
 
-void * lgethead( List list )
-{
-    if( list && list->head )
-    {
+void * lgethead(List list) {
+    if (list && list->head) {
+        _lock( &list->lock);
         void * data = list->head->data;
         LNode node = list->head;
         list->head = list->head->next;
-        Free( node );
-        if( list->head ) list->head->prev = NULL;
+        Free(node);
+        if (list->head)
+            list->head->prev = NULL;
         list->size--;
-        if( !list->size ) list->tail = NULL;
+        if (!list->size)
+            list->tail = NULL;
+        _unlock(list->lock);
         return data;
     }
     return NULL;
 }
 
-void * lgettail( List list )
-{
-    if( list && list->tail )
-    {
+void * lgettail(List list) {
+    if (list && list->tail) {
+        _lock( &list->lock);
         void * data = list->tail->data;
         LNode node = list->tail;
         list->tail = list->tail->prev;
-        Free( node );
-        if( list->tail ) list->tail->next = NULL;
+        Free(node);
+        if (list->tail)
+            list->tail->next = NULL;
         list->size--;
-        if( !list->size ) list->head = NULL;
+        if (!list->size)
+            list->head = NULL;
+        _unlock(list->lock);
         return data;
     }
     return NULL;
 }
 
-void ldeltail( List list )
-{
-    void * data = lgettail( list );
-    if( data && list->destructor ) list->destructor( data );
+void ldeltail(List list) {
+    void * data = lgettail(list);
+    if (data && list->destructor)
+        list->destructor(data);
 }
 
-void ldelhead( List list )
-{
-    void * data = lgethead( list );
-    if( data && list->destructor ) list->destructor( data );
+void ldelhead(List list) {
+    void * data = lgethead(list);
+    if (data && list->destructor)
+        list->destructor(data);
 }
 
-void * ltail( List list )
-{
+void * ltail(List list) {
     return (list && list->tail) ? list->tail->data : NULL;
 }
-void * lhead( List list )
-{
+void * lhead(List list) {
     return (list && list->head) ? list->head->data : NULL;
 }
 
--- list.h
+++ list.h
@@ -32,6 +32,7 @@ typedef struct _List
     LNode cursor;
     L_destructor destructor;
     size_t size;
+    int lock;
 }*List;
 
 List lcreate( L_destructor destructor );
