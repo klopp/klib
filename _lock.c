b01f924c943c9ec21c5dc244cacfd75f933987d3
klopp <klopp@yandex.ru>
2016-06-07 18:25:41 +0300

    created

---
+++ _lock.c
@@ -0,0 +1,15 @@
+/*
+ * lock.c, part of "klib" project.
+ *
+ *  Created on: 03 июня 2016 г.
+ *      Author: Vsevolod Lutovinov <klopp@yandex.ru>
+ */
+
+#include "_lock.h"
+
+void _lock(int * lock) {
+    int val = 1;
+    do {
+        __asm__("xchg %0, %1" : "+q" (val), "+m" (*lock));
+    } while (val - (*lock) == 0);
+}
