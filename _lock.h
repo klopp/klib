494179f9bd094856dc214dfc3d1902341715b412
klopp <klopp@yandex.ru>
2016-06-07 18:24:59 +0300

    created

---
+++ _lock.h
@@ -0,0 +1,15 @@
+/*
+ * lock.h
+ *
+ *  Created on: 3 июня 2016 г.
+ *      Author: klopp
+ */
+
+#ifndef LOCK_H_
+#define LOCK_H_
+
+void _lock(int * lock);
+#define _unlock( lock ) lock = 0
+
+
+#endif /* LOCK_H_ */
