4a735e3e6a09355d8ee3fbbe2ef2ed39cb6232e0
klopp <klopp@yandex.ru>
2016-06-09 17:40:20 +0300

    added

---
+++ st_trace.c
@@ -0,0 +1,148 @@
+/*
+ * st_trace.c, part of "klib" project.
+ *
+ *  Created on: 30.05.2016, 16:54
+ *      Author: Vsevolod Lutovinov <klopp@yandex.ru>
+ */
+
+#include "st_trace.h"
+#include "list.h"
+#include "_lock.h"
+#include "sig.h"
+
+#ifndef NDEBUG
+
+/* ---------------------------------------------------------------------------*/
+typedef struct {
+    char name[_ST_NAME_MAX + 1];
+    char file[_ST_FILE_MAX + 1];
+    char arg[_ST_ARG_MAX + 1];
+    char ret[_ST_RET_MAX + 1];
+    unsigned line;
+
+} _st_trace;
+
+static Stack _st_stack = NULL;
+static size_t _st_over = 0;
+int _st_lock = 0;
+
+/* ---------------------------------------------------------------------------*/
+static void _st_signal(int signo) {
+    switch (signo) {
+        case SIGSEGV:
+        case SIGILL:
+        case SIGABRT:
+        case SIGBUS:
+        case SIGFPE:
+        case SIGSYS:
+            fprintf( stderr, "\nGot signal %s, stack trace:\n\n",
+                    signal_name(signo));
+            st_dump(NULL, NULL, " ", 2);
+            break;
+        default:
+            break;
+    }
+    signal(signo, SIG_DFL);
+}
+
+/* ---------------------------------------------------------------------------*/
+static void _st_down(void) {
+    sdestroy(_st_stack);
+}
+
+/* ---------------------------------------------------------------------------*/
+void _st_init(void) {
+    if (!_st_stack) {
+        _st_stack = screate(lambda( void, (void * data ) { Free(data); } ));
+        atexit(_st_down);
+        signal( SIGSEGV, _st_signal);
+        signal( SIGABRT, _st_signal);
+        signal( SIGILL, _st_signal);
+        signal( SIGBUS, _st_signal);
+        signal( SIGFPE, _st_signal);
+        signal( SIGSYS, _st_signal);
+    }
+}
+
+/* ---------------------------------------------------------------------------*/
+size_t _st_push(const char * ret, const char * name, const char * arg,
+        const char * file, unsigned line) {
+
+    _lock(&_st_lock);
+    _st_trace * data = Calloc(sizeof(_st_trace), 1);
+
+    strncpy(data->ret, ret, _ST_RET_MAX);
+    strncpy(data->name, name, _ST_NAME_MAX);
+    strncpy(data->file, file, _ST_FILE_MAX);
+    strncpy(data->arg, arg, _ST_ARG_MAX);
+    data->line = line;
+
+    size_t i = strlen(data->arg);
+
+    if (*arg == '(') {
+        memmove(data->arg, data->arg + 1, i);
+        i--;
+    }
+
+    if (i && *(data->arg + i - 1) == ')')
+        *(data->arg + i - 1) = 0;
+
+    if (!spush(_st_stack, data)) {
+        _st_over++;
+    }
+
+    _unlock(_st_lock);
+    return _st_stack->size;
+}
+
+/* ---------------------------------------------------------------------------*/
+void _st_pop(void) {
+
+    _lock(&_st_lock);
+    if (_st_over) {
+        _st_over--;
+    }
+    else {
+        Free(spop(_st_stack));
+    }
+    _unlock(_st_lock);
+}
+
+/* ---------------------------------------------------------------------------*/
+static void _default_dumper(const char * ret, const char * name,
+        const char * arg, const char * file, unsigned line, FILE * handle) {
+    fprintf(handle, "%s %s(%s) at %s, line %u\n", ret, name, arg, file, line);
+}
+
+/* ---------------------------------------------------------------------------*/
+void st_dump(const st_dumper dumper, FILE * file, const char * tab, int numbers) {
+
+    _lock(&_st_lock);
+    size_t n = 1;
+    size_t i = _st_stack->size;
+    st_dumper _dumper = dumper ? dumper : _default_dumper;
+    const char * _tab = tab ? tab : "";
+    FILE * _file = file ? file : stderr;
+    LNode node = _st_stack->tail;
+
+    while (node) {
+        _st_trace * data = node->data;
+        node = node->prev;
+
+        size_t j = 0;
+        if (numbers) {
+            fprintf(_file, numbers == 2 ? "%zu. " : "%zu ", n);
+            n++;
+        }
+        while (tab && *_tab && j < _st_stack->size - i) {
+            fprintf(_file, "%s", _tab);
+            j++;
+        }
+        i--;
+        _dumper(data->ret, data->name, data->arg, data->file, data->line,
+                _file);
+    }
+    _unlock(_st_lock);
+}
+
+#endif
---
+++ st_trace.h
@@ -0,0 +1,103 @@
+/*
+ * st_trace.h, part of "klib" project.
+ *
+ *  Created on: 30.05.2016, 16:54
+ *      Author: Vsevolod Lutovinov <klopp@yandex.ru>
+ */
+
+#ifndef ST_TRACE_H_
+#define ST_TRACE_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#ifndef NDEBUG
+
+#include <limits.h>
+#include <stdio.h>
+#include <string.h>
+#include "va_arg.h"
+
+#define return(...)    JOIN(return,__VA_NARG__(__VA_ARGS__))(__VA_ARGS__)
+
+#define _ST_MAX         0x400
+#define _ST_FILE_MAX    PATH_MAX
+#define _ST_NAME_MAX    0x100
+#define _ST_ARG_MAX     0x400
+#define _ST_RET_MAX     0x100
+
+typedef void (*st_dumper)(const char * ret, const char * name, const char * arg,
+        const char * file, unsigned line, FILE * handle);
+/*
+ * dumper:
+ *   if NULL default dumper will be used
+ * file:
+ *   if NULL STDERR will be used
+ * tab:
+ *   if NULL no indentation will be used
+ * number:
+ *   1 - print line numbers (1, 2, 3 ...)
+ *   2 - print line numbers with dot (1., 2., 3. ...)
+ */
+void st_dump(const st_dumper dumper, FILE * file, const char * tab, int numbers);
+
+/*
+ * internal functions:
+ */
+size_t _st_push(const char * ret, const char * name, const char * arg,
+        const char * file, unsigned line);
+void _st_pop(void);
+void _st_init(void);
+
+/*
+ *  ret function( args )
+ */
+#define f_start( ret, func, args )  ret func args { \
+                                    _st_push( #ret, #func, #args,  \
+                                    __FILE__, __LINE__ );
+
+#define return0()                   { _st_pop(); return; }
+#define return1( ret )              { _st_pop(); return(ret); }
+
+#else /* NDEBUG */
+
+/*
+ *  ret function( args )
+ */
+#define f_start( ret, func, args )  ret function args {
+
+#define _return0()                  { return; }
+#define _return1( ret )             { return(ret); }
+
+#define _st_dump(...)               (void)0
+
+#endif /* NDEBUG */
+
+/*
+ * void function( args )
+ */
+#define f_vstart( function, args )  f_start( void, function, args )
+
+/*
+ * ret function()
+ */
+#define f_startv( ret, function )   f_start( ret, function, () )
+
+/*
+ * void function()
+ */
+#define f_vstartv( function )       f_start( void, function, () )
+
+#define _main()                     int main(int argc, char **argv, char **env) { \
+                                    _st_init(); \
+                                    _st_push( "int", "main", "argc, argv, env", \
+                                    __FILE__, __LINE__); \
+                                    unused(argc); unused(argv); unused(env);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ST_TRACE_H_ */
