7970cda5a410df05604ac17a542a17a3e622e73d
klopp <klopp@yandex.ru>
2016-06-16 15:24:30 +0300

    const char * __ex_msgs[] (const added)

--- trycatch.c
+++ trycatch.c
@@ -8,7 +8,7 @@
 
 jmp_buf __ex_env[TRYCATCH_MAX + 1];
 __ex_types  __ex_type[TRYCATCH_MAX + 1] = { Exception };
-char *__ex_msgs[TRYCATCH_MAX + 1] = { ( char * ) 0 };
+const char *__ex_msgs[TRYCATCH_MAX + 1] = { ( char * ) 0 };
 unsigned int __ex_idx = 0;
 
 /*
