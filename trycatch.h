52e07ead14e9d4aba6c1ad37744562a4a44a8552
klopp <klopp@yandex.ru>
2016-06-16 16:50:18 +0300

    __ex_have_msg, __ex_with_msg

--- trycatch.h
+++ trycatch.h
@@ -17,21 +17,34 @@ extern "C" {
 #define TRYCATCH_MAX    32
 
 #define try \
-    assert(__ex_idx < TRYCATCH_MAX); __ex_idx++; \
-        if(!(__ex_type[__ex_idx-1] = setjmp(__ex_env[__ex_idx-1])))
+        assert(__ex_idx < TRYCATCH_MAX); __ex_idx++; \
+            if(!(__ex_type[__ex_idx-1] = setjmp(__ex_env[__ex_idx-1])))
 
 #define catch(X) \
-    else if(((X)+Exception) == Exception || __ex_type[__ex_idx-1] == ((X)+Exception))
+        else if(((X)+Exception) == Exception || \
+                __ex_type[__ex_idx-1] == ((X)+Exception))
 
 #define throw(X,...) \
-    __ex_msgs[__ex_idx-1] = (__VA_ARGS__+0), longjmp(__ex_env[__ex_idx-1], (X))
+        __ex_msgs[__ex_idx-1] = (__VA_ARGS__+0), \
+            longjmp(__ex_env[__ex_idx-1], (X))
 
 /*
  *  finally block MUST be called, always!
  */
-#define finally assert(__ex_idx); __ex_idx--
+#define finally \
+        assert(__ex_idx); __ex_idx--; __ex_type[__ex_idx] = Exception; \
+            __ex_msgs[__ex_idx] = (const char *)0
 
-#define __ex_msg ((__ex_idx && __ex_idx < TRYCATCH_MAX) ? __ex_msgs[__ex_idx-1] : (const char *)0)
+#if defined(NDEBUG)
+# define __ex_have_msg (__ex_idx && __ex_idx < TRYCATCH_MAX ) ? \
+         __ex_msgs[__ex_idx-1] : 0
+#else
+# define __ex_have_msg __ex_msgs[__ex_idx-1]
+#endif
+
+#define __ex_with_msg   __ex_have_msg
+
+#define __ex_msg        __ex_msgs[__ex_idx-1]
 
 typedef enum {
     Exception = 0,
