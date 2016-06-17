f678abefa1abcec82e2f94e824a846d7458c325d
klopp <klopp@yandex.ru>
2016-06-17 19:42:00 +0300

    int types

--- config.h
+++ config.h
@@ -67,6 +67,26 @@
 # define Mlock( ptr )           (void)ptr
 #endif
 
+#ifdef _MSC_VER
+typedef __int16 int16_t;
+typedef unsigned __int16 uint16_t;
+typedef __int32 int32_t;
+typedef unsigned __int32 uint32_t;
+typedef __int64 int64_t;
+typedef unsigned __int64 uint64_t;
+#else
+# include <stdint.h>
+#endif
+
+#define     i16                 int16_t
+#define     ui16                uint16_t
+
+#define     i32                 int32_t
+#define     ui32                uint32_t
+
+#define     i64                 int64_t
+#define     ui64                uint64_t
+
 #define forever()       for(;;)
 #define unused( var )   ((void)var)
 
