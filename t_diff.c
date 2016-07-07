04ba068ac7e3329fde58566e9d3a2e724992ec44
klopp <klopp@yandex.ru>
2016-07-07 04:01:22 +0300

    astyle

--- t_diff.c
+++ t_diff.c
@@ -6,14 +6,16 @@
 #include "t_diff.h"
 
 /* ---------------------------------------------------------------------------*/
-unsigned long t_diff(struct timeval *start, struct timeval *end) {
-
+unsigned long t_diff( struct timeval *start, struct timeval *end ) {
     struct timeval lend;
-    if (!end) {
-        gettimeofday(&lend, 0);
+
+    if( !end ) {
+        gettimeofday( &lend, 0 );
         end = &lend;
     }
-    return (end->tv_sec - start->tv_sec) * 1000000 + end->tv_usec - start->tv_usec;
+
+    return ( end->tv_sec - start->tv_sec ) * 1000000 + end->tv_usec -
+           start->tv_usec;
 }
 
 /* ---------------------------------------------------------------------------*/
