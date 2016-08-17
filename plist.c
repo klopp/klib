0092d9b5a5a2688b78d5be02285a50bb4982e726
klopp <klopp@yandex.ru>
2016-08-17 14:05:09 +0300

    string.h added

--- plist.c
+++ plist.c
@@ -6,8 +6,10 @@
  */
 
 #include "plist.h"
+#include <string.h>
 
-void pair_Delete( void *ptr ) {
+void pair_Delete( void *ptr )
+{
     if( ptr ) {
         Pair pair = ( Pair )ptr;
         Free( pair->first );
@@ -16,44 +18,56 @@ void pair_Delete( void *ptr ) {
     }
 }
 
-const char *plget( PList list, const char *key ) {
+const char *plget( PList list, const char *key )
+{
     char c = key[0];
     Pair pair = plfirst( list );
+
     while( pair ) {
         if( c == pair->first[0] && !strcmp( key, pair->first ) ) {
             return pair->second;
         }
+
         pair = plnext( list );
     }
+
     return NULL;
 }
 
-Pair pair_Create( const char *first, const char *second ) {
+Pair pair_Create( const char *first, const char *second )
+{
     Pair pair = Calloc( sizeof( struct _Pair ), 1 );
+
     if( pair ) {
         if( first ) {
             pair->first = Strdup( first );
+
             if( !pair->first ) {
                 pair_Delete( pair );
                 return NULL;
             }
         }
+
         if( second ) {
             pair->second = Strdup( second );
+
             if( !pair->second ) {
                 pair_Delete( pair );
                 return NULL;
             }
         }
     }
+
     return pair;
 }
 
-PList plcreate( void ) {
+PList plcreate( void )
+{
     return lcreate( pair_Delete );
 }
 
-Pair pladd( List list, const char *first, const char *second ) {
+Pair pladd( List list, const char *first, const char *second )
+{
     Pair pair = pair_Create( first, second );
     return pair ? ladd( list, pair ) : NULL;
 }
